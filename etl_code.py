a66b19cea03352cffe490921c5e67c169b93e405
Noor Asma <nasma9521@gmail.com>
2025-04-26 16:34:37 +0300

    remove comment

--- etl_code.py
+++ etl_code.py
@@ -6,8 +6,6 @@ from datetime import datetime
 log_file = "log_file"
 
 target_file = "transformed_data.csv"
-
-
 def extract_from_csv(file_to_process):
     df = pd.read_csv(file_to_process)
     df[['height', 'weight']] = df[['height', 'weight']].astype(float)
@@ -98,5 +96,4 @@ load_data(target_file, transformed_data)
 
 log_progress("Load phase Ended")
 
-#  todo continue with the error of concatenation
 
